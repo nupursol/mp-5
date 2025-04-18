5087a735caf54df7d7ed4ee52b4fb301fa72b53c
nupursol <nupur@bu.edu>
2025-04-18 17:40:20 -0400

    fixed dynamic routing bug

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -1,11 +1,11 @@
 import getCollection, { URLS_COLLECTION } from "@/db";
 import { notFound, redirect } from "next/navigation";
 
-type Params = {
+export default async function AliasRedirect({
+                                                params,
+                                            }: {
     params: { alias: string };
-};
-
-export default async function AliasRedirect({ params }: Params) {
+})  {
     const collection = await getCollection(URLS_COLLECTION); //get the collection
     const doc = await collection.findOne({ alias: params.alias }); //find the alias  entered
     if (!doc) { // try to find the alias user entered
