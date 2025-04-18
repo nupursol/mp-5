c9957ffec5af0b36e41caec5148769e059a731fd
nupursol <nupur@bu.edu>
2025-04-18 18:49:59 -0400

    trying to model after discussion

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -1,15 +1,13 @@
 import getCollection, { URLS_COLLECTION } from "@/db";
 import { notFound, redirect } from "next/navigation";
 
-export default async function AliasRedirect({
-                                                params,
-                                            }: {
-    params: { alias: string };
-}) {
-    console.log("PARAMS", params.alias);
+export default async function AliasRedirect(
+    { params }: { params: Promise<{ alias: string }> }
+) {
+    const { alias } = await params;
 
     const collection = await getCollection(URLS_COLLECTION);
-    const doc = await collection.findOne({ alias: params.alias });
+    const doc = await collection.findOne({ alias });
 
     if (!doc) {
         notFound();
