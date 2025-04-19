fe01705effe9cccc4b4a1125c978f0742f48d7ce
nupursol <nupur@bu.edu>
2025-04-18 20:00:03 -0400

    FINALLY fixed dynamic routing error by matching it to discussion

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -2,16 +2,13 @@ import getCollection, { URLS_COLLECTION } from "@/db";
 import { notFound, redirect } from "next/navigation";
 
 export default async function AliasRedirect(
-    { params }: { params: Promise<{ alias: string }> }
+    { params }: { params: Promise<{ alias: string }> } //changed this to match discussion format after hours of debugging
 ) {
     const { alias } = await params;
-
     const collection = await getCollection(URLS_COLLECTION);
     const doc = await collection.findOne({ alias });
-
     if (!doc) {
         notFound();
     }
-
     redirect(doc.longUrl);
 }
